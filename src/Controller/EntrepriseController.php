06ac73d26c52c8bcffaaad5aef0c890c2392efd6
gitcomptemoi <miorintsoa@yahoo.fr>
2019-12-11 16:04:48 +0100

    Ajout des fonctions de suppression et modif et add

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,8 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -12,6 +22,56 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class EntrepriseController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    /**
+     * @Route("/add", name="entreprise_add")
+     * @Route("/{id}/edit", name="entreprise_edit")
+     */
+    public function addEntreprise(Request $request, Entreprise $entreprise = null)
+    {
+        if(!$entreprise){
+            $entreprise = new Entreprise();
+        }
+        $form = $this->createFormBuilder($entreprise)
+            ->add('raisonSociale', TextType::class)
+            ->add('siret', TextType::class)
+            ->add('adresse', TextType::class)
+            ->add('cp', TextType::class)
+            ->add('ville', TextType::class)
+            ->add('Valider', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->entityManager->persist($entreprise);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('entreprise_index');
+        }
+
+        return $this->render('entreprise/add_edit.html.twig', [
+            'form' => $form->createView(),
+            'editMode' => $entreprise->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/remove", name="entreprise_remove")
+     */
+    public function removeEntreprise($id)
+    {
+        $adminEntreprises = $this->entityManager->getRepository(Entreprise::class)
+                                             ->find($id);
+        $this->entityManager->remove($adminEntreprises);
+        $this->entityManager->flush();                                                     
+        return $this->redirectToRoute('entreprise_index');
+    }
+    
     /**
      * @Route("/", name="entreprise_index")
      */
